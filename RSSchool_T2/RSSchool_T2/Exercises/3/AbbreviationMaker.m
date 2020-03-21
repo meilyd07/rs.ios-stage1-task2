73a8106b2b735b31905a72fb2b76cb43c7817bca
MeilyD07 <meilyd07@gmail.com>
2020-03-21 03:35:01 +0300

    feat: add abbreviation maker

--- RSSchool_T2/RSSchool_T2/Exercises/3/AbbreviationMaker.m
+++ RSSchool_T2/RSSchool_T2/Exercises/3/AbbreviationMaker.m
@@ -3,6 +3,31 @@
 @implementation AbbreviationMaker
 // Complete the abbreviation function below.
 - (NSString *) abbreviationFromA:(NSString *)a toB:(NSString *)b {
-    return @"Test";
+    
+    NSString *uppercasedA = [a uppercaseString];
+    NSMutableOrderedSet *bSet = [self convertStringToSet:b];
+    NSMutableOrderedSet *aSet = [self convertStringToSet:uppercasedA];
+    
+    [aSet intersectOrderedSet:bSet];
+    
+    if ([aSet isEqualToOrderedSet:bSet]) {
+        return @"YES";
+    } else {
+        return @"NO";
+    }
+}
+
+- (NSMutableOrderedSet *)convertStringToSet:(NSString *)string {
+    
+    NSMutableArray *characters = [[NSMutableArray alloc] initWithCapacity:[string length]];
+    for (int i=0; i < [string length]; i++) {
+        NSString *ichar  = [NSString stringWithFormat:@"%c", [string characterAtIndex:i]];
+        [characters addObject:ichar];
+    }
+    
+    NSMutableOrderedSet *setCharacters = [[NSOrderedSet orderedSetWithArray:characters] mutableCopy];
+    [characters release];
+    
+    return [setCharacters autorelease];
 }
 @end
